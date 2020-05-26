d36d253d26c2c0daca3b012305e843ce45b7e125
伊藤脩一郎 <ahuro110.play@gmail.com>
2020-05-26 16:53:40 +0900

    finish_comment_favorite

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :tweets, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_tweets, through: :favorites, source: :tweet
+  has_many :comments, dependent: :destroy
+end
